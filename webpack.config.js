8ed0ec1dc98217c07dd72dadd74eaa8103b66319
zezadas <>
2021-11-19 15:26:02 +0000

    fix xhr url

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,12 @@ module.exports = {
     ]
   },
   devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    },
     host: '0.0.0.0',
+    public: 'xmas2021.sefod.eu',
     contentBase: './dist',
     hot: true,
      disableHostCheck: true
