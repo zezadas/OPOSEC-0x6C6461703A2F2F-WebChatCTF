35f27b4aef7db52b735a94ec86141611e598702a
zezadas <>
2021-11-19 17:16:50 +0000

    chrome headless bot

--- dockerzadas/chrome_decapitado/example.js
+++ dockerzadas/chrome_decapitado/example.js
@@ -1,19 +1,27 @@
-/*const puppeteer = require('puppeteer');
-
-(async() => {
-  const browser = await puppeteer.launch();
-  console.log(await browser.version());
-  await browser.close();
-})();*/
-
-
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://google.com');
-  //  await page.screenshot({ path: 'example.png' });
+function delay(time) {
+   return new Promise(function(resolve) { 
+       setTimeout(resolve, time)
+   });
+}
 
-  await browser.close();
+(async () => {
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const page = await browser.newPage();
+    await page.goto('https://xmas2021.sefod.eu/');
+    await delay(10000);
+    await page.evaluate(_ => {
+    window.flag="flag{ThisOneWasEasy}";
+    var texto = document.getElementById("message-input-box");
+    var botao = document.getElementById("send-button");
+    var setValue = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
+    setValue.call(texto, '/nick botzadas');
+
+    var e = new Event('change', { bubbles: true });
+        texto.dispatchEvent(e);
+        botao.click();
+    });
+//  await browser.close();
 })();
+
