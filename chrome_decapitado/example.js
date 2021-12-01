0fa288a318dc4735d7ab915ff829eb00349954a3
zezadas <>
2021-12-01 13:07:24 +0000

    add condition to use testing env

--- chrome_decapitado/example.js
+++ chrome_decapitado/example.js
@@ -9,8 +9,18 @@ function delay(time) {
 (async () => {
     const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
-    await page.goto('https://xmas2021.sefod.eu/');
-    await delay(10000);
+    await delay(5000);
+    var url="";
+    if (process.env.SOCKETURL === ""){
+            url = 'http://xmas2021.sefod.eu';
+    }
+    else{
+            url = 'http://clientzadas:8080';
+    }
+    console.log(url);
+    await page.goto(url);
+    await delay(5000);
+    console.log("continuing");
     await page.evaluate(_ => {
     window.flag="flag{This_Flag_Is_Easier_Than_Rabanadas}";
     var texto = document.getElementById("message-input-box");
